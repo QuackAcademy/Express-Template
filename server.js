ea39082ea060e02add4afd0244d0bfc7d9c9f7db
Jordan Miller <jordan.j.miller@live.com>
2021-01-03 01:31:27 -0800

    Added error middleware call

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const authenticate = require('./auth/authenticate-middleware.js');
 const authRouter = require('./auth/auth-router.js');
 const userRouter = require('./routers/users-router.js');
 
+const { handleError } = require('./middleware/error-middleware.js');
+
 const server = express();
 
 server.use(helmet());
@@ -16,6 +18,8 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', authenticate, userRouter);
+
+server.use((err, req, res, next) => { handleError(err, req, res); });
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'hi'});
