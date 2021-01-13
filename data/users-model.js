4173f95b3fdb45d6b0b736e10f1fe50b359cc68a
Jordan Miller <jordan.j.miller@live.com>
2021-01-13 15:38:15 -0800

    Cleaned up file and fixed a function not returning full data as expected

--- data/users-model.js
+++ data/users-model.js
@@ -1,61 +1,24 @@
 const db = require('./db-config');
 
-module.exports = {
-    findBy,
-    findByMultiple,
-    findById,
-    add,
-    update,
-    remove,
-}
+module.exports = { findBy, findByMultiple, findById, add, update, remove }
 
-function findBy(value){
-    return db('users')
-        .where(value)
-        .first();
-}
+function findBy(value){ return db('users').where(value).first(); }
 
-function findByMultiple(value1, value2){
-    // console.log(value1, value2)
-    return db('users')
-        .where({...value1, ...value2})
-}
+function findByMultiple(value1, value2){ return db('users').where({...value1, ...value2}) }
 
-function add(user){
-    return db('users')
-    .insert(user, 'id');
-}
+function add(user){ return db('users').insert(user, 'id'); }
 
-function findById(id){
-    return db('users as u')
-    .where({'u.id': id})
-    .select('u.id', 'u.email',)
-    .first();
-}
+function findById(id){ return db('users').where({id}).first(); }
 
-function update(id, user){
-    return db('users')
-    .where({id})
-    .update({...user});
-}
+function update(id, user){ return db('users').where({id}).update({...user}); }
 
 async function remove(id){
     await db.transaction(async trx => {
         try{
-             await trx('users')
-            .where({id});
-
-            const userDeleted = await trx('users')
-            .where({id})
-            .del();
-            
-            if(!userDeleted){
-                throw 'Error deleting user'
-            }
-
+            await trx('users').where({id});
+            const userDeleted = await trx('users').where({id}).del();
+            if(!userDeleted){ throw 'Error deleting user' }
             return true;
-        }catch(err){
-            throw err;
-        }
+        } catch(err){ throw err; }
     });
 }
