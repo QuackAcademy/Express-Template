32de71c0771fa7f8689919f02f8ad93571984622
Jordan Miller <jordan.j.miller@live.com>
2021-01-14 01:21:26 -0800

    Added PUT /users/user tests

--- tests/users-router-tests.js
+++ tests/users-router-tests.js
@@ -13,30 +13,15 @@ const createTestUser = async () => {
 module.exports = () => {
     createTestUser();
     
-    //#region users router
-        // case `/users get /user 404`: status = 404; responseObj = { message: `User with id ${req.user.id} not found` }; break;
-        // case `/users get /:id 400`: status = 400; responseObj = { message: `Param id must be a number. Received: ${req.params.id}` }; break;
-        // case `/users get /:id 404`: status = 404; responseObj = { message: `User with id ${req.user.id} not found` }; break;
-        // case `/users put /user 400`: status = 400; responseObj = { message: `Current password is required` }; break;
-        // case `/users put /user 400-2`: status = 400; responseObj = { message: `Username must only contain characters A-Z, _, and 0-9. Username must start with a letter` }; break;
-        // case `/users put /user 403`: status = 403; responseObj = { message: `Invalid credentials` }; break;
-        // case `/users put /user 404`: status = 404; responseObj = { message: `User not found` }; break;
-        // case `/users put /user 409`: status = 409; responseObj = { message: `Username '${req.username}' is already in use` }; break;
-        // case `/users put /user 409-2`: status = 409; responseObj = { message: `Email '${req.email}' is already in use` }; break;
-        // case `/users delete /user 400`: status = 400; responseObj = { message: 'Password is required to delete user' }; break;
-        // case `/users delete /user 403`: status = 403; responseObj = { message: 'Invalid credentials' }; break;
-    //#endregion
-
-    
     // console.log(`res.status: ${res.status}, message: ${res.body.message}`)
     // console.log(`res.status: ${res.status}, body: `, res.body)
-
+    
     it('Should require authorization', async () => {
         const res = await request(server).get(`/api/users/${testUserID}`);
         expect(res.status).toBe(400);
         expect(res.body.message).toEqual('No token was provided');
     });
-
+    
     describe('GET /users/:id', () => {
         it('Should return status 200 and correct data for a valid request', async () => {
             const res = await request(server).get(`/api/users/${testUserID}`).set({'authorization': token});
@@ -58,5 +43,78 @@ module.exports = () => {
             expect(res.status).toBe(404);
             expect(res.body.message).toEqual('User with id 404045034530495345 not found');
         });
+    });
+    describe('GET /users/user', () => {
+        // case `/users get /user 404`: status = 404; responseObj = { message: `User with id ${req.user.id} not found` }; break;
+        
+    });
+    describe('GET /users/all', () => {
+        
+    });
+    describe('PUT /users/user', () => {
+        it('Should return status 400 if password is missing', async () => {
+            const res = await request(server).put('/api/users/user')
+            .send({ email: 'fdhfsdfsd@gmail.com' }).set({'authorization': token})
+            expect(res.status).toBe(400);
+            expect(res.body.message).toBe('Current password is required')
+        })
+        it('Should return status 400 if nothing was provided to update', async () => {
+            const res = await request(server).put('/api/users/user')
+            .send({ password: 'testO023123#@#adSD' }).set({'authorization': token})
+            expect(res.status).toBe(400);
+            expect(res.body.message).toBe('No new values were sent')
+        })
+        it('Should return status 400 and reject invalid usernames', async () => {
+            const invalidCharacters = await request(server).put('/api/users/user')
+            .send({ username: "QHtestuse@@@@@r12039c1243124", password: "testO023123#@#adSD" }).set({'authorization': token})
+            expect(invalidCharacters.status).toBe(400);
+            expect(invalidCharacters.body.message).toBe('Username must only contain characters A-Z, _, and 0-9. Username must start with a letter')
+
+            const numberFirst = await request(server).put('/api/users/user')
+            .send({ username: "1QHtestuser12039c1243124", password: "testO023123#@#adSD" }).set({'authorization': token})
+            expect(numberFirst.status).toBe(400);
+            expect(numberFirst.body.message).toBe('Username must only contain characters A-Z, _, and 0-9. Username must start with a letter')
+        })
+        it('Should return status 403 if password is incorrect', async () => {
+            const res = await request(server).put('/api/users/user')
+            .send({ password: 'notyourpassword', fullName: 'name changed' }).set({'authorization': token});
+            expect(res.status).toBe(403);
+            expect(res.body.message).toBe('Invalid credentials')
+        })
+        it("Should return status 404 if token user isn't found", async () => {
+            // I'll have to test this by providing a valid (can be decoded with secret and has proper values) token of a deleted user
+            const invalidToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijo2LCJpYXQiOjE2MTA2MTU4NjMsImV4cCI6MTYxMTY5NTg2M30.W33UfE8AUdI5AoIMXk-NsuJfn_UxAbxm_1OuoUkXINE';
+            const res = await request(server).put('/api/users/user')
+            .send({ password: 'notyourpassword', fullName: 'name changed' }).set({'authorization': invalidToken});
+            expect(res.status).toBe(404);
+            expect(res.body.message).toBe('User not found')
+        })
+        it('Should return status 409 for conflicts', async () => {
+            const usernameInUse = await request(server).put('/api/users/user')
+            .send({ email: 'quackquack222@gmail.com', username: "QHtestuser",	password: "testO023123#@#adSD", fullName: "quack test user" })
+            .set({'authorization': token})
+            expect(usernameInUse.status).toBe(409);
+            expect(usernameInUse.body.message).toBe(`Username 'QHtestuser' is already in use`)
+            const emailInUse = await request(server).put('/api/users/user')
+            .send({ email: 'quackquack@gmail.com', username: "QHtestuser222", password: "testO023123#@#adSD", fullName: "quack test user" })
+            .set({'authorization': token})
+            expect(emailInUse.status).toBe(409);
+            expect(emailInUse.body.message).toBe(`Email 'quackquack@gmail.com' is already in use`)
+        })
+        it('Should return status 200 and return new values if user was updated', async () => {
+            const res = await request(server).put('/api/users/user')
+            .send({ email: 'quackquack2@gmail.com', username: "QHtestuser2", password: "testO023123#@#adSD", newPassword: '2testO023123#@#adSD', fullName: 'testerson2' })
+            .set({'authorization': token})
+            expect(res.status).toBe(200);
+            expect(res.body).toEqual({ id: testUserID, email: 'quackquack2@gmail.com', username: "qhtestuser2", fullName: 'testerson2' });
+        })
+        it('Should have updated password after last test', async () => {
+            const loginResponse = await request(server).post('/api/auth/login')
+            .send({ username: "QHtestuser2", password: "2testO023123#@#adSD", });
+            expect(loginResponse.status).toBe(200);
+        })
+    });
+    describe('DELETE /users/user', () => {
+        // case `/users delete /user 403`: status = 403; responseObj = { message: 'Invalid credentials' }; break;
     });
 }
