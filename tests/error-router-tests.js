8f1e71e6b173545dd19d1c155bf3b3b7e97bdf9f
Jordan Miller <jordan.j.miller@live.com>
2021-01-16 21:19:28 -0800

    Started on error tests

---
+++ tests/error-router-tests.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const server = require('../server.js');
+
+let token = '';
+const createTestUser = async () => {
+    await request(server).post('/api/auth/register').send({ email: 'quackquack@gmail.com', username: "QHtestuser", password: "testO023123#@#adSD", fullName: 'testerson' });
+    const loginResponse = await request(server).post('/api/auth/login').send({ username: "QHtestuser", password: "testO023123#@#adSD", });
+    testUserID = loginResponse.body.user?.id;
+    token = loginResponse.body.token;
+}
+
+module.exports = () => {
+    createTestUser();
+
+    it('Should require authorization', async () => {
+        const res = await request(server).post(`/api/errors/getByQuery`);
+        expect(res.status).toBe(400);
+        expect(res.body.message).toEqual('No token was provided');
+    });
+    describe('POST /errors/getByQuery', () => {
+        // it('Should return status 201 if registered', async () => {
+        //     const res = await request(server).post('/api/auth/register')
+        //     .send({ email: "QHtestuser120391243124@gmail.com", username: "QHtestuser12039c1243124", password: "testO023123#@#adSD", fullName: "quack test user" })
+        //     expect(res.status).toBe(201);
+        //     expect(res.type).toMatch(/json/i);
+        //     expect(res.body.message).toContain('Successfully added user #')
+        // })
+    })
+}
--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,6 +1,7 @@
 const serverTests = require('./server-tests.js');
 const authTests = require ('./auth-router-tests.js');
 const userTests = require ('./users-router-tests.js');
+const errorTests = require ('./users-router-tests.js');
 const db = require('../data/db-config');
 
 const setup = async () => { await db('users').truncate(); }
@@ -9,3 +10,4 @@ setup();
 describe('Server tests', serverTests);
 describe('Auth router tests', authTests);
 describe('Users router tests', userTests);
+// describe('Error router tests', errorTests);
