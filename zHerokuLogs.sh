ec7734a924a400db40d199aff0a78a2ea06203b5
Jordan Miller <jordan.j.miller@live.com>
2021-02-09 14:40:36 -0800

    Updated to use deployed template name

--- zHerokuLogs.sh
+++ zHerokuLogs.sh
@@ -2,5 +2,5 @@
 
 #resize window: printf '\033[8;(height);(width)t'  setting super high will windowed fullscreen it
 printf '\033[8;75;300t'
-heroku logs -a changeappname -t
+heroku logs -a quacktemplate -t
 $SHELL
