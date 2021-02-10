664db364b591736b5dd7da78c437dc62a218354d
Jordan Miller <jordan.j.miller@live.com>
2021-02-09 17:50:30 -0800

    Downgraded pg and knex to attempt to fix heroku pg connection issue. looks like there is potentially a breaking change in pg versions > 8

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
     migrations: { directory: './data/migrations' },
     seeds: { directory: './data/seeds' } 
   },
