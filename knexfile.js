3daccc65b7cac00ad9317b352ea8bb3ddfbdbbaf
Jordan Miller <jordan.j.miller@live.com>
2021-02-09 17:17:47 -0800

    Attempting to fix heroku postgres connection issue

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    sslmode: 'require',
+    ssl: { rejectUnauthorized: false },
     migrations: { directory: './data/migrations' },
     seeds: { directory: './data/seeds' } 
   },
